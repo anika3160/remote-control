4a5670c9c12f277393182c8e619083621fa9c95c
Anna Surahatava <anika3160@gmail.com>
2023-01-23 18:58:00 +0100

    fix: fix streams

--- src/ws/stream.ts
+++ src/ws/stream.ts
@@ -6,15 +6,15 @@ const wsServer = new WebSocketServer({ port: WS_PORT })
 wsServer.on('connection', async (ws) => {
   ws.send('Hi!')
   console.log('Client connected.')
-  const duplex = createWebSocketStream(ws, { decodeStrings: false, encoding: 'utf-8', defaultEncoding: 'utf-8' })
-  duplex.pipe(appStream).pipe(duplex)
+  const duplex = createWebSocketStream(ws, { encoding: 'utf-8', defaultEncoding: 'utf-8' })
+
   duplex.on('error', (err) => {
     console.log(err)
   })
   duplex.on('close', () => {
     console.log('the duplex channel has closed')
   })
-
+  duplex.pipe(appStream).pipe(duplex)
   ws.on('close', () => {
     console.log('Connection closed.')
     process.exit()
--- src/ws/transform.ts
+++ src/ws/transform.ts
@@ -2,12 +2,11 @@ import { Transform } from 'node:stream'
 import app from '../app/index.js';
 
 const appStream = new Transform({
-  decodeStrings: false,
   async transform(chunk, encoding, callback) {
-    console.log('chunk:', chunk)
-    const buff = await app(chunk)
-    console.log('buff:', encoding, buff)
-    this.push(buff)
+    // console.log('chunk:', chunk)
+    const str = await app(chunk)
+    // console.log('chunk:', chunk)
+    this.push(str)
     callback()
   },
 })
